1d6bada043bda328b971003a27bb0f32a25135bb
Julio Lora <juliolora1999@yahoo.com>
2021-09-17 15:51:42 -0400

    Added comment

--- HW_01/classify_triangles.py
+++ HW_01/classify_triangles.py
@@ -2,25 +2,13 @@
 The primary goal of this file is to be able to classify a triangle based on the given inputs for the side lengths
 
 @author: Julio Lora
+I pledge my honor that I have abided by the Stevens Honor System.
 """
 
-import unittest     # this makes Python unittest module available
+import unittest
 
 def classify_triangle(a,b,c):
-    """
-    
-    This function returns a string with the type of triangle from three  values
-    corresponding to the lengths of the three sides of the Triangle.
-    
-    return:
-        If all three sides are equal, return 'Equilateral'
-        If exactly one pair of sides are equal, return 'Isoceles'
-        If no pair of  sides are equal, return 'Scalene'
-        If not a valid triangle, then return 'NotATriangle'
-        If the sum of any two sides equals the squate of the third side, then return 'Right'
-        
-        
-    """
+    # code has bugs included for the purpose of testing
     
     #first, I will ensure the input given is valid
     if not(isinstance(a,int)):
